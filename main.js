adb3fd06b254ccbf341f2e93c28b8158f89d824b
cam <cameron.a.sax@gmail.com>
2022-09-25 16:33:09 -0400

    commits

---
+++ main.js
@@ -0,0 +1,14 @@
+function evenNumbers(array, number) {
+    let newArr = []
+    array.filter(x=> {
+      if (x % 2 === 0) {
+        newArr.push(x)
+      }
+    })
+    return newArr.splice(-number)
+  }
+
+
+console.log(evenNumbers([2,4,6], 2)) // => [4,6]
+console.log(evenNumbers([1,3,5,6,8,11,12,25,26,28], 3)) // => [12, 26, 28]
+console.log(evenNumbers([11,12,5], 2)) // => 4
